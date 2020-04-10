ab7f92e7355c602994720b1519fdcffd50a8a6ce
jaeju.jang <jjj0611@gmail.com>
2020-04-10 15:04:33 +0900

    fix. Order 관련 버그 수정
    	- 주문 개수가 음수가 되거나 전체 주문 개수가 100건 이상인 경우 예외 발생
    	- 예외는 발생하나 데이터가 그대로 들어 있는 문제 해결

--- src/main/java/domain/order/Order.java
+++ src/main/java/domain/order/Order.java
@@ -4,6 +4,7 @@ import domain.menu.Menu;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Order {
     private static final int MAX_ORDER_COUNT = 100;
@@ -11,25 +12,29 @@ public class Order {
 
     private Map<Menu, Integer> menuCount = new HashMap<>();
 
-    public void add(Menu menu, int count) {
-        menuCount.computeIfPresent(menu, (key, value) -> value + count);
-        menuCount.putIfAbsent(menu, count);
-        validateMenuCount(menu);
-        validateTotalMenuCount();
+    public void add(Menu menu, int additionalCount) {
+        validateMenuCount(menu, additionalCount);
+        validateTotalMenuCount(additionalCount);
+        menuCount.computeIfPresent(menu, (key, value) -> value + additionalCount);
+        menuCount.putIfAbsent(menu, additionalCount);
     }
 
-    private void validateMenuCount(Menu menu) {
-        if (menuCount.get(menu) < MINIMUM_COUNT) {
+    private void validateMenuCount(Menu menu, int additionalCount) {
+        Integer count = menuCount.get(menu);
+        if (!Objects.isNull(count) && count + additionalCount < MINIMUM_COUNT) {
+            throw new IllegalArgumentException("메뉴 개수는 음수일 수 없습니다.");
+        }
+        if (Objects.isNull(count) && additionalCount < MINIMUM_COUNT) {
             throw new IllegalArgumentException("메뉴 개수는 음수일 수 없습니다.");
         }
     }
 
-    private void validateTotalMenuCount() {
+    private void validateTotalMenuCount(int additionalCount) {
         int totalCount = menuCount.values()
                 .stream()
                 .reduce(Integer::sum)
                 .orElse(0);
-        if (totalCount >= MAX_ORDER_COUNT) {
+        if (totalCount + additionalCount >= MAX_ORDER_COUNT) {
             throw new IllegalArgumentException("주문은 최대 99개까지 가능합니다.");
         }
     }
